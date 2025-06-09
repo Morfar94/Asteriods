460666b434ae55a12ac2aff6958ac57082cd10eb
Morfar94 <Ravn94@live.dk>
2025-06-09 10:42:39 +0200

    display opsat

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
-
+import constants
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
